05ac9e4cdeef293b5305d73179e92e9d2d67b340
John Vasileff <john@vasileff.com>
2020-02-04 15:05:34 -0500

    Attempt to minimize intermittent test failures

--- compiler-java/test/src/org/eclipse/ceylon/compiler/java/test/ConcurrentScheduler.java
+++ compiler-java/test/src/org/eclipse/ceylon/compiler/java/test/ConcurrentScheduler.java
@@ -30,7 +30,10 @@ import org.junit.runners.BlockJUnit4ClassRunner;
 import org.junit.runners.model.RunnerScheduler;
 
 public class ConcurrentScheduler implements RunnerScheduler {
-    private final ThreadPoolExecutor tpool = (ThreadPoolExecutor)Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors(),
+    private final ThreadPoolExecutor tpool = (ThreadPoolExecutor)Executors.newFixedThreadPool(
+            // limit to max 4 threads to reduce frequency of race condition test failures :/
+            // including the "testJar" test
+            Math.min(4, Runtime.getRuntime().availableProcessors()),
             new ThreadFactory() {
                 int ii = 0;
                 @Override
